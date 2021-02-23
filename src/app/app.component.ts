6e1553d62bb68be4ca8b4179df640217b3df49bc
ryan98100 <c14180115@john.petra.ac.id>
2021-02-23 19:13:38 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,4 +18,27 @@ export class AppComponent  {
     ["*","*","*","*","*"],
     ["*","*","*","*","*"],
   ];
+
+  click() {
+    if(this.game!=true){
+      if (this.giliran) {
+        if (this.board[this.X][this.Y] == "") {
+          this.board[this.X][this.Y] = "0";
+          this.giliran = false;
+        } else {
+          alert("Sudah diisi musuh!");
+        }
+      } else {
+        if (this.board[this.X][this.Y] == "") {
+          this.board[this.X][this.Y] = "1";
+          this.giliran = true;
+        } else {
+          alert("Sudah diisi musuh!");
+        }
+      }
+    }
+    else{
+      alert("Buyar");
+    }
+  }
 }
