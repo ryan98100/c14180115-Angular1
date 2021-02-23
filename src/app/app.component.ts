28a9843ab1f5b237dbbba49c46c72342ca1d8d8e
ryan98100 <c14180115@john.petra.ac.id>
2021-02-23 19:20:06 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,4 +41,288 @@ export class AppComponent  {
       alert("Buyar");
     }
   }
+
+  menag(){
+    if (
+      this.board[0][0] == "0" &&
+      this.board[0][1] == "0" &&
+      this.board[0][2] == "0" &&
+      this.board[0][3] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[0][1] == "0" &&
+      this.board[0][2] == "0" &&
+      this.board[0][3] == "0" &&
+      this.board[0][4] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[1][0] == "0" &&
+      this.board[1][1] == "0" &&
+      this.board[1][2] == "0" &&
+      this.board[1][3] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[1][1] == "0" &&
+      this.board[1][2] == "0" &&
+      this.board[1][3] == "0" &&
+      this.board[1][4] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[2][0] == "0" &&
+      this.board[2][1] == "0" &&
+      this.board[2][2] == "0" &&
+      this.board[2][3] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[2][1] == "0" &&
+      this.board[2][2] == "0" &&
+      this.board[2][3] == "0" &&
+      this.board[2][4] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[3][0] == "0" &&
+      this.board[3][1] == "0" &&
+      this.board[3][2] == "0" &&
+      this.board[3][3] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[3][1] == "0" &&
+      this.board[3][2] == "0" &&
+      this.board[3][3] == "0" &&
+      this.board[3][4] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[4][0] == "0" &&
+      this.board[4][1] == "0" &&
+      this.board[4][2] == "0" &&
+      this.board[4][3] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[4][1] == "0" &&
+      this.board[4][2] == "0" &&
+      this.board[4][3] == "0" &&
+      this.board[4][4] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[0][0] == "0" &&
+      this.board[1][0] == "0" &&
+      this.board[2][0] == "0" &&
+      this.board[3][0] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[1][0] == "0" &&
+      this.board[2][0] == "0" &&
+      this.board[3][0] == "0" &&
+      this.board[4][0] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[0][1] == "0" &&
+      this.board[1][1] == "0" &&
+      this.board[2][1] == "0" &&
+      this.board[3][1] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[1][1] == "0" &&
+      this.board[2][1] == "0" &&
+      this.board[3][1] == "0" &&
+      this.board[4][1] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[0][2] == "0" &&
+      this.board[1][2] == "0" &&
+      this.board[2][2] == "0" &&
+      this.board[3][2] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[1][2] == "0" &&
+      this.board[2][2] == "0" &&
+      this.board[3][2] == "0" &&
+      this.board[4][2] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[0][3] == "0" &&
+      this.board[1][3] == "0" &&
+      this.board[2][3] == "0" &&
+      this.board[3][3] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[1][3] == "0" &&
+      this.board[2][3] == "0" &&
+      this.board[3][3] == "0" &&
+      this.board[4][3] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[0][4] == "0" &&
+      this.board[1][4] == "0" &&
+      this.board[2][4] == "0" &&
+      this.board[3][4] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[1][4] == "0" &&
+      this.board[2][4] == "0" &&
+      this.board[3][4] == "0" &&
+      this.board[4][4] == "0"
+    ) {
+      alert("Player 1 win!");
+    } else if (
+      this.board[0][0] == "1" &&
+      this.board[0][1] == "1" &&
+      this.board[0][2] == "1" &&
+      this.board[0][3] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[0][1] == "1" &&
+      this.board[0][2] == "1" &&
+      this.board[0][3] == "1" &&
+      this.board[0][4] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[1][0] == "1" &&
+      this.board[1][1] == "1" &&
+      this.board[1][2] == "1" &&
+      this.board[1][3] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[1][1] == "1" &&
+      this.board[1][2] == "1" &&
+      this.board[1][3] == "1" &&
+      this.board[1][4] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[2][0] == "1" &&
+      this.board[2][1] == "1" &&
+      this.board[2][2] == "1" &&
+      this.board[2][3] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[2][1] == "1" &&
+      this.board[2][2] == "1" &&
+      this.board[2][3] == "1" &&
+      this.board[2][4] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[3][0] == "1" &&
+      this.board[3][1] == "1" &&
+      this.board[3][2] == "1" &&
+      this.board[3][3] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[3][1] == "1" &&
+      this.board[3][2] == "1" &&
+      this.board[3][3] == "1" &&
+      this.board[3][4] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[4][0] == "1" &&
+      this.board[4][1] == "1" &&
+      this.board[4][2] == "1" &&
+      this.board[4][3] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[4][1] == "1" &&
+      this.board[4][2] == "1" &&
+      this.board[4][3] == "1" &&
+      this.board[4][4] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[0][0] == "1" &&
+      this.board[1][0] == "1" &&
+      this.board[2][0] == "1" &&
+      this.board[3][0] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[1][0] == "1" &&
+      this.board[2][0] == "1" &&
+      this.board[3][0] == "1" &&
+      this.board[4][0] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[0][1] == "1" &&
+      this.board[1][1] == "1" &&
+      this.board[2][1] == "1" &&
+      this.board[3][1] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[1][1] == "1" &&
+      this.board[2][1] == "1" &&
+      this.board[3][1] == "1" &&
+      this.board[4][1] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[0][2] == "1" &&
+      this.board[1][2] == "1" &&
+      this.board[2][2] == "1" &&
+      this.board[3][2] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[1][2] == "1" &&
+      this.board[2][2] == "1" &&
+      this.board[3][2] == "1" &&
+      this.board[4][2] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[0][3] == "1" &&
+      this.board[1][3] == "1" &&
+      this.board[2][3] == "1" &&
+      this.board[3][3] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[1][3] == "1" &&
+      this.board[2][3] == "1" &&
+      this.board[3][3] == "1" &&
+      this.board[4][3] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[0][4] == "1" &&
+      this.board[1][4] == "1" &&
+      this.board[2][4] == "1" &&
+      this.board[3][4] == "1"
+    ) {
+      alert("Player 2 win!");
+    } else if (
+      this.board[1][4] == "1" &&
+      this.board[2][4] == "1" &&
+      this.board[3][4] == "1" &&
+      this.board[4][4] == "1"
+    ) {
+      alert("Player 2 win!");
+    }
+  }
 }
